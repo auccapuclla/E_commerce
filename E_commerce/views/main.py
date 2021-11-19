1e2ab61ab1491b536096eddc2407bb244d9f7356
tenma18 <jhonjairorojasortiz@gmail.com>
2021-11-18 22:55:39 -0500

    Organización de carpetas en app principal

--- E_commerce/views/main.py
+++ E_commerce/views/main.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.template import Template, Context
+from django.template import loader
 
 
 def home(request):
-    document_html = open('C:/Users/jhon_/Google Drive UNI/Scripts/Django/E_commerce/E_commerce/templates/main.html')
-    template = Template(document_html.read())
-    document_html.close()
+    # document_html = open('C:/Users/jhon_/Google Drive UNI/Scripts/Django/E_commerce/E_commerce/templates/main.html')
+    # template = Template(document_html.read())
+    # document_html.close()
     ctx = Context({'partner': 'Fabio'})
     document = template.render(ctx)
     return HttpResponse(document)
