9f9cee902b62a77a07ae9890ff06465d21ae3a96
Joel Auccapuclla <auccapuclla2013@gmail.com>
2021-11-19 11:46:33 +0800

    users add

--- E_commerce/urls.py
+++ E_commerce/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views
 
 from .views.main import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
-
+    path('register/', views.register, name='register'),
 ]
--- E_commerce/views/main.py
+++ E_commerce/views/main.py
@@ -3,9 +3,10 @@ from django.template import Template, Context
 
 
 def home(request):
-    document_html = open('C:/Users/jhon_/Google Drive UNI/Scripts/Django/E_commerce/E_commerce/templates/main.html')
-    template = Template(document_html.read())
-    document_html.close()
-    ctx = Context({'partner': 'Fabio'})
-    document = template.render(ctx)
+    # document_html = open('../templates/main.html')
+    # template = Template(document_html.read())
+    # document_html.close()
+    # ctx = Context({'partner': 'Fabio'})
+    # document = template.render(ctx)
+    document = 'ddd'
     return HttpResponse(document)
